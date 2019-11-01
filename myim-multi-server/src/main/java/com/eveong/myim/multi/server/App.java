5aa7169e8d76a91f654f653d10c9b1e4411650c9
方云鹤 <842241090@qq.com>
2019-11-01 18:50:50 +0800

    feature:新增模块

---
+++ myim-multi-server/src/main/java/com/eveong/myim/multi/server/App.java
@@ -0,0 +1,13 @@
+package com.eveong.myim.multi.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
